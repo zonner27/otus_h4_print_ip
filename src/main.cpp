7c5a675beaeddeb7b3cd10acff5389c13020b131
zonner27 <zonner2785@gmail.com>
2019-08-27 15:24:12 +0300

    second

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <array>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <list>
+
+
+template <typename T>
+decltype(std::cout << std::declval<T>(), void())
+print_ip(T value)
+{
+    constexpr int size = sizeof(value);
+    std::array<unsigned char, size> bytes;
+    std::memcpy(&bytes, &value, size);
+    for (unsigned int i = size - 1; i != 0; --i)
+        std::cout << std::to_string(bytes[i]) << ".";
+    std::cout << std::to_string(bytes[0]);
+    std::cout << std::endl;
+}
+
+void print_ip(std::string value)
+{
+    std::cout << value << std::endl;
+}
+
+template <typename T>
+decltype(begin(std::declval<T>()), end(std::declval<T>()), void())
+print_ip(T container)
+{
+    for (auto it = std::begin(container); it != std::end(container); ++it)
+        {
+            std::cout << std::to_string(*it);
+
+            if (std::next(it) != std::end(container))
+                std::cout << ".";
+        }
+
+    std::cout << std::endl;
+}
 
 int main()
 {
-    std::cout << "print_ip " << std::endl;
+    try {
+        print_ip(char(-1));
+        print_ip(short(0));
+        print_ip(int(2130706433));
+        print_ip(long(8875824491850138409));
+        print_ip(std::string("192.168.1.1"));
+        print_ip(std::vector<int>{1, 2, 3, 4});
+        print_ip(std::list<char>{5, 6, 7, 8});
+
+    } catch (std::exception& e) {
+
+        std::cout << e.what() << std::endl;
+    }
+
 
     return 0;
 }
